31afe0b8597ec7ad517524f0d7789372e9de2bb0
Mohammad <mstariran@gmail.com>
2019-07-15 04:07:13 -0700

    add Expense Model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField(default=0)
+    user = models.ForeignKey(User, 'cascade')
